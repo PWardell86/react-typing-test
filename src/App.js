0a33a75f0dc798e5a81dd33e497979cad31ca680
Peter <peterwardell86@gmail.com>
2024-09-04 00:15:25 -0400

    allow file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import React from 'react';
-import { getUser } from './ServerAPI';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TypingTestPage from './components/TypingTestPage';
-import UserOptionsPage from './components/UserOptionsPage';
-import LeaderboardPage from './components/LeaderboardPage';
-import FeedbackModal from './components/FeedbackModal';
+import "./App.css";
+import React from "react";
+import { getUser } from "./ServerAPI";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TypingTestPage from "./components/TypingTestPage";
+import UserOptionsPage from "./components/UserOptionsPage";
+import LeaderboardPage from "./components/LeaderboardPage";
+import FeedbackModal from "./components/FeedbackModal";
+import FileUpload from "./components/FileUpload";
+
 class App extends React.Component {
   state = { user: null };
 
   componentDidMount() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       getUser(token)
         .then((response) => {
           const body = document.body;
-          body.classList.remove('light-theme', 'dark-theme');
-          document.body.classList.add(response.data.default_theme + '-theme');
+          body.classList.remove("light-theme", "dark-theme");
+          document.body.classList.add(response.data.default_theme + "-theme");
           this.setState({ user: response.data });
-        }).catch((error) => {
-          localStorage.removeItem('token');
+        })
+        .catch((error) => {
+          localStorage.removeItem("token");
           console.log(error);
           return null;
         });
@@ -31,17 +34,23 @@ class App extends React.Component {
       <Router>
         <Routes>
           <Route index element={<TypingTestPage user={this.state.user} />} />
-          <Route path="/options" element={<UserOptionsPage user={this.state.user} />} />
-          <Route path="/leaderboards" element={<LeaderboardPage user={this.state.user} />} />
+          <Route
+            path="/options"
+            element={<UserOptionsPage user={this.state.user} />}
+          />
+          <Route
+            path="/leaderboards"
+            element={<LeaderboardPage user={this.state.user} />}
+          />
+          <Route path="/uploadFile" element={<FileUpload />} />
           <Route path="*" element={<h1>404 haha</h1>} />
         </Routes>
         <footer>
           <FeedbackModal />
         </footer>
       </Router>
-
     );
-  }
+  };
 }
 
 export default App;
--- src/ServerAPI.js
+++ src/ServerAPI.js
@@ -1,74 +1,90 @@
-import axios from 'axios';
+import axios from "axios";
 
-const ADDRESS = 'peterwardell.site';
+const ADDRESS = "peterwardell.site";
 const BACKEND = `https://${ADDRESS}/api`;
 
 function signUp(username, password) {
-  return axios.post(BACKEND + '/createuser', {
-    username: username,
-    password: password
-  }).then(() => {
-    login(username, password)
-  });
+  return axios
+    .post(BACKEND + "/createuser", {
+      username: username,
+      password: password,
+    })
+    .then(() => {
+      login(username, password);
+    });
 }
 
 function login(username, password) {
-  return axios.post(BACKEND + '/login', {
-    username: username,
-    password: password
-  }).then((response) => {
-    localStorage.setItem('token', response.data.token);
-    window.location.reload();
-  });
+  return axios
+    .post(BACKEND + "/login", {
+      username: username,
+      password: password,
+    })
+    .then((response) => {
+      localStorage.setItem("token", response.data.token);
+      window.location.reload();
+    });
 }
 
 function logout() {
-  localStorage.removeItem('token');
+  localStorage.removeItem("token");
   window.location.reload();
 }
 
 function getUser(token) {
-  return axios.post(BACKEND + '/getuser', {
-    token: token
+  return axios.post(BACKEND + "/getuser", {
+    token: token,
   });
 }
 
 function getScores(token, maxRows) {
-  return axios.post(BACKEND + '/getscores', {
+  return axios.post(BACKEND + "/getscores", {
     token: token,
-    max_rows: maxRows
+    max_rows: maxRows,
   });
 }
 
 function setUserOptions(token, options) {
-  return axios.post(BACKEND + '/setuseroptions', {
+  return axios.post(BACKEND + "/setuseroptions", {
     token: token,
-    user_options: options
+    user_options: options,
   });
 }
 
 function addScore(token, scoreJSON) {
-  return axios.post(BACKEND + '/addscore', {
+  return axios.post(BACKEND + "/addscore", {
     token: token,
-    ...scoreJSON
+    ...scoreJSON,
   });
 }
 
 function saveFeedback(feedback) {
-  return axios.post(BACKEND + '/savefeedback', {
+  return axios.post(BACKEND + "/savefeedback", {
     feedback: feedback,
-  })
+  });
 }
 
 function getLeaderboard(type, maxRows) {
-  return axios.post(BACKEND + '/getleaderboard', {
+  return axios.post(BACKEND + "/getleaderboard", {
     type: type,
-    max_rows: maxRows
+    max_rows: maxRows,
   });
 }
 
 function getParagraph() {
-  return axios.get(BACKEND + '/getparagraph');
+  return axios.get(BACKEND + "/getparagraph");
 }
 
-export { signUp, login, getUser, getScores, addScore, saveFeedback, logout, setUserOptions, getLeaderboard , getParagraph };
+export {
+  signUp,
+  login,
+  getUser,
+  getScores,
+  addScore,
+  saveFeedback,
+  logout,
+  setUserOptions,
+  getLeaderboard,
+  getParagraph,
+  BACKEND,
+};
