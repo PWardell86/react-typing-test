12305a7f3fb3e820f271eaf28c281bcada99309a
Peter <peterwardell86@gmail.com>
2024-09-04 10:56:52 -0400

    fixinf

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,35 +1,13 @@
 import React, { useState } from "react";
 import { BACKEND } from "../ServerAPI";
-import axios from "axios";
 
 const FileUpload = () => {
-  const [progress, setProgress] = useState(0);
-  const uploadZipFile = async (event) => {
-    event.preventDefault();
-    const file = event.target.uploadFile.files[0];
-    try {
-      const formData = new FormData();
-      formData.append("zipFile", file);
-
-      const response = await axios.post(BACKEND, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        onUploadProgress: (progressEvent) => {
-          setProgress(
-            Math.round((progressEvent.loaded / progressEvent.total) * 100)
-          );
-        },
-      });
-
-      console.log("File uploaded successfully:", response.data);
-    } catch (error) {
-      console.error("Error uploading file:", error);
-    }
-  };
-
   return (
-    <form onSubmit={uploadZipFile}>
+    <form
+      action={BACKEND + "/uploadFile"}
+      method="POST"
+      enctype="multipart/form-data"
+    >
       <h2>Resumable File Upload</h2>
       <input type="file" accept=".zip" name="uploadFile" />
       <button type="submit">Upload File</button>
