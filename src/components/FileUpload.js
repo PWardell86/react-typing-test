e3a81af617832398773a19390397fece910c5de0
Peter <peterwardell86@gmail.com>
2024-09-04 11:01:17 -0400

    fixinf

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import { BACKEND } from "../ServerAPI";
 
 const FileUpload = () => {
+  const uploadFile = (e) => {
+    e.preventDefault();
+    let file = e.target.uploadFile.files[0];
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    axios
+      .post(BACKEND + "/uploadFile", formData)
+      .then((res) => console.log(res))
+      .catch((err) => console.warn(err));
+  };
   return (
-    <form
-      action={BACKEND + "/uploadFile"}
-      method="POST"
-      enctype="multipart/form-data"
-    >
+    <form onSubmit={uploadFile}>
       <h2>Resumable File Upload</h2>
       <input type="file" accept=".zip" name="uploadFile" />
       <button type="submit">Upload File</button>
